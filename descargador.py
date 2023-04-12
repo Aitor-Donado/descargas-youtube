2a7fb9aaa1282110e5af9a2a9e986e07d91a4d07
Aitor <aitordonado@gmail.com>
2023-04-12 19:52:53 +0200

    Proyecto completo

---
+++ descargador.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+
+direccion = input("Escribe la dirección del vídeo: ")
+
+yt = YouTube(direccion)
+
+print(yt.title)
+
+musical = input("¿Es un vídeo de música? [y/n]: ")
+
+if musical == "y":
+    audio = yt.streams.get_audio_only()
+    pais = input("Alemán, griego, ruso: [a/g/r]: ")
+    if pais == "g":
+        audio.download("/home/laptop/Música/Griega")
+    elif pais == "a":
+        audio.download("/home/laptop/Música/Alemana")
+    elif pais == "r":
+        audio.download("/home/laptop/Música/Rusa")
+    else:
+        audio.download("/home/laptop/Música")
+else:
+    video = yt.streams.get_by_resolution("720p")
+    video.download("/home/laptop/Vídeos")
