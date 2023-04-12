683222a1826d062789f6ef47613ce9c38978070d
Aitor <aitordonado@gmail.com>
2023-04-12 19:16:22 +0200

    opcion audio por input

--- index.py
+++ index.py
@@ -1,15 +1,14 @@
 from pytube import YouTube
 
 direccion = input("Dirección del vídeo:")
-
+audio = input("¿Quieres gurdar sólo el audio? [s]: ") == "s"
 yt = YouTube(direccion)
 
 print(yt.title)
-"""
-audio = yt.streams.get_audio_only()
-audio.download("/home/laptop/Música")
-"""
 
-video = yt.streams.get_by_resolution("720p")
-video.download("/home/laptop/Música")
-
+if audio:
+    audio = yt.streams.get_audio_only()
+    audio.download("/home/laptop/Música")
+else:
+    video = yt.streams.get_by_resolution("720p")
+    video.download("/home/laptop/Vídeos")
