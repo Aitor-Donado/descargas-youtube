983f33aee0db3a8b568561caebf1ff8a1c5526fd
Aitor <aitordonado@gmail.com>
2023-04-18 19:19:35 +0200

    Aviso del título y si es audio o video

--- index.py
+++ index.py
@@ -4,11 +4,13 @@ direccion = input("Dirección del vídeo:")
 audio = input("¿Quieres gurdar sólo el audio? [s]: ") == "s"
 yt = YouTube(direccion)
 
-print(yt.title)
+
 
 if audio:
+    print("Guardando el audio: ", yt.title)
     audio = yt.streams.get_audio_only()
     audio.download("/home/laptop/Música")
 else:
+    print("Guardando el video: ", yt.title)
     video = yt.streams.get_by_resolution("720p")
     video.download("/home/laptop/Vídeos")
